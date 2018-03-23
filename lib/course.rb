43e2b6c2a9f21a67cfde84a28dcbee8307c37f09
Christine Cua <cloudycua@gmail.com>
2018-03-23 16:27:13 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -14,5 +14,5 @@ class Course
   def self.reset_all
     @@all.clear
   end
-  
+
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
 
-require 'pry'
-
 require_relative './course.rb'
 
 class Scraper
@@ -12,17 +10,17 @@ class Scraper
       binding.pry
     end
 
-#    def print_courses
-#    self.make_courses
-#    Course.all.each do |course|
-#      if course.title
-#        puts "Title: #{course.title}"
-#        puts "  Schedule: #{course.schedule}"
-#        puts "  Description: #{course.description}"
-#      end
-#    end
-#  end
+    def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
 
 end
 
-Scraper.new.get_page
+Scraper.new.print_courses
